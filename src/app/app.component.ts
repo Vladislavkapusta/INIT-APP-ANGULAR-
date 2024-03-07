aa591b3a675d9b08e0dd0fb9c7766907d1f43148
Vladislavkapusta <KapustinVi21@st.ithub.ru>
2024-03-07 18:38:37 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,16 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'myapp';
+  name = "wrapper";
+  number = 11;
+  array = [1,2,3,4,5];
+  users = [
+    {id: 1, name: 'Alex', age: 18},
+    {id: 2, name: 'Steven', age: 19},
+    {id: 3, name: 'Dima', age: 17},
+    {id: 4, name: 'Neena', age: 15},
+  ]
 }
